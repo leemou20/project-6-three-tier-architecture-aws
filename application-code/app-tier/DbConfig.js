81282791241cf6167f02f1100a3d46308d6e02d5
MOULI S <leemouofficial@gmail.com>
2026-02-03 17:49:13 +0530

    Delete application-code directory

--- application-code/app-tier/DbConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.freeze({
-    DB_HOST : 'database-1.cp0ucok46iwe.ap-south-1.rds.amazonaws.com',
-    DB_USER : 'admin',
-    DB_PWD : 'admin123#',
-    DB_DATABASE : 'database-1'
-});
